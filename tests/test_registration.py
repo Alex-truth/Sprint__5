5c5b903a6fbbd516f0d23aaf06cec3cb926cc44e
Alexey Makalkin <ponikslip123@yandex.ru>
2025-01-12 18:13:20 +0400

    import random for registration

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -4,14 +4,16 @@ from selenium.webdriver.common.by import By
 from locators import Locators
 from urls import Urls
 from data import Data
+from utils import RandomDataGenerator
 
 class TestRegistration:
 # регистрация пользователя с валидными данными
     def test_successful_registration(self, driver, wait):
         driver.get(Urls.REGISTER_PAGE)
         wait.until(expected_conditions.element_to_be_clickable((By.XPATH, Locators.INPUT_NAME)))
+        random_email = RandomDataGenerator.generate_random_email()
         driver.find_element(By.XPATH, Locators.INPUT_NAME).send_keys(Data.NAME)
-        driver.find_element(By.XPATH, Locators.INPUT_EMAIL).send_keys(Data.EMAIL)
+        driver.find_element(By.XPATH, Locators.INPUT_EMAIL).send_keys(random_email)
         driver.find_element(By.XPATH, Locators.INPUT_PASSWORD).send_keys(Data.PASSWORD)
         driver.find_element(By.XPATH, Locators.REGISTRATION_BUTN).click()
         wait.until(expected_conditions.url_to_be(Urls.LOGIN_PAGE))
@@ -20,8 +22,9 @@ class TestRegistration:
     def test_registration_err_short_password(self, driver, wait):
         driver.get(Urls.REGISTER_PAGE)
         wait.until(expected_conditions.element_to_be_clickable((By.XPATH, Locators.INPUT_NAME)))
+        random_email = RandomDataGenerator.generate_random_email()
         driver.find_element(By.XPATH, Locators.INPUT_NAME).send_keys(Data.NAME)
-        driver.find_element(By.XPATH, Locators.INPUT_EMAIL).send_keys(Data.EMAIL)
+        driver.find_element(By.XPATH, Locators.INPUT_EMAIL).send_keys(random_email)
         driver.find_element(By.XPATH, Locators.INPUT_PASSWORD).send_keys(Data.INVALID_PWD)
         driver.find_element(By.XPATH, Locators.REGISTRATION_BUTN).click()
         wait.until(expected_conditions.visibility_of_element_located((By.XPATH, Locators.INVALID_PWD_ERR)))
